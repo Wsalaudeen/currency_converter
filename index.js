9c1b6d543427584e7cb627ef3d5fb2441e0ab2c6
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2023-10-30 11:46:37 +0100

    commit message

---
+++ index.js
@@ -0,0 +1,27 @@
+// creating dom elements
+const currencyFirstEl = document.getElementById("currency-first");
+const worthFirstEl = document.getElementById("worth-first");
+const currencySecondEl = document.getElementById("currency-second");
+const worthSecondEl = document.getElementById("worth-second");
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+
+  // funtion to be listened to  in an addEventListener
+function updateRate() {
+  fetch(
+    `https://v6.exchangerate-api.com/v6/1904ea22b9a1f5720f0e1bd8/latest/${currencyFirstEl.value}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const rate = data.conversion_rates[currencySecondEl.value];
+      exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${
+        rate + " " + currencySecondEl.value
+      }`;
+      worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
+    });
+}
+updateRate();
+// creating an addEventlistener
+currencyFirstEl.addEventListener("change", updateRate);
+worthFirstEl.addEventListener("change", updateRate);
+currencySecondEl.addEventListener("change", updateRate);
